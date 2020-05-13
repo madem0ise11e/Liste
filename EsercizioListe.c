fe35aa23918e67e91aa9cc519b864383d8c5f7a5
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-05-13 20:26:45 +0200

    adjust

--- EsercizioListe.c
+++ EsercizioListe.c
@@ -1,50 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXLEN 50
 
 struct studente {
   char cognome[MAXLEN];
-  char nome[MAXLEN];
-  int matricola;
-  float mediaVoti;
+//  char nome[MAXLEN];
+//  int matricola;
+//  float mediaVoti;
   struct studente *next;
 };
 
-struct studente *addInTesta(struct studente *s, int i);
+struct studente *addInTesta(struct studente *t, char cog[]);
 //struct studente *addInCoda(struct studente *s[], int i);
-void stampaLista(struct studente* top);
+void stampaLista(struct studente* s);
 
 int main(){
   int i=0, n=0;
-  struct studente *s=NULL;
+  char cog;
+  struct studente *s = NULL;
   printf("Quanti studenti vuoi aggiungere? ");
   scanf("%d", &n);
   for(i=0; i<n; ++i){
-    s = addInTesta(s,i);
+    addInTesta(s,cog);
   //  s2 = addInCoda(s2,i);
   }
   printf("stampa Lista in testa");
   stampaLista(s);
-  printf("stampa Lista in coda");
+//  printf("stampa Lista in coda");
 //  stampaLista(s2);
 return 0;
 }
 
-struct studente *addInTesta(struct studente *s, int i){
+struct studente *addInTesta(struct studente *t, char cog[]){
   struct studente *tmp = (struct studente *)malloc(sizeof(struct studente));
-  tmp->cognome = i;
-  tmp->next=s;
-  s=tmp;
-  return s;
+//  printf("Inserisci cognome: ");
+  //scanf("%s", cog);
+  tmp->cognome = cog;
+  tmp->next=t;
+  t=tmp;
+  return t;
 }
 
-void stampaLista(struct studente* top){
+void stampaLista(struct studente* s){
   struct studente *tmp;
-  tmp=top;
+  tmp=s;
   printf("\nStampa Lista: \n");
   while(tmp!=NULL){
-    printf("%s %s %d %f", tmp->cognome, tmp->nome, tmp->matricola, tmp->mediaVoti);
+    printf("%s", tmp->cognome/*, tmp->nome, tmp->matricola, tmp->mediaVoti*/);
     tmp=tmp->next;
   }
 }
